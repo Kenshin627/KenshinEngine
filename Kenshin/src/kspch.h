53bffe2c6d945640a4182a27b2a8a4639db9b23b
slayer_0 <87zhaoxiaobo@163.com>
2023-04-11 18:02:15 +0800

    add pch file

---
+++ Kenshin/src/kspch.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <memory>
+#include <functional>
+#include <algorithm>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+
+
+#ifdef KS_PLATFORM_WINDOWS
+#include <window.h>
+#endif // KS_PLATFORM_WINDOWS
